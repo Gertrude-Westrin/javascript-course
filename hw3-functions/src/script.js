506e3ac225b7ca7f460596610d278e18aaf7dc0e
Gertrude Westrin <gertrude.westrin@gmail.com>
2020-04-13 13:08:24 +0200

    Functions homework

---
+++ hw3-functions/src/script.js
@@ -0,0 +1,49 @@
+//Question1
+
+//function oddOrEven() { //determine the function, that is funtion name=odd or even which is what I want to do (find out if a number is an odd or even numebr).
+    //let number = +prompt("Give me a number"); //ask user for a number
+    //if (number % 2 === 0) { 
+        //console.log(` "Even" `);
+    //} else {
+        //console.log(` "Odd" `);
+    //} 
+    //set the condition to determine whether of not the given number is "odd" or "even" 
+//} 
+//console.log(oddOrEven()); //display the function argument. I tried having return in my code but it wasn´t doing anything so I removed it. Should this question have return?
+
+
+//Question2
+
+//function removeVowels(string) { //declare the function to remove vowels from string.
+    //let vowels = { //declare the vowels using boolean
+        //"a": true,
+        //"e": true,
+        //"i": true,
+        //"o": true,
+        //"u": true,  
+    //};
+    //let result = ""; //declare result
+
+    //for (let i = 0; i < string.length; i++) { 
+        //using the for loop set parameter for cutting in combination with if condition so that, the code can also remove vowels in capital letters.
+        //let letter = string [i].toLowerCase(); //set code to turn uppercase to lowercase letters
+        //if (!vowels[letter]) {
+            //result += string[i];
+        //}
+    //};
+    //return result; //reture function   
+//};
+//console.log(removeVowels("This website is for loosers, LOL")); //display comment with vowels removed.
+
+
+//Question3
+
+function repeatString () { //declare the function
+    let string = "hello"; //define string
+    return string = string.repeat (5); //call the set function
+}
+console.log(repeatString());//print 
+
+
+
+
