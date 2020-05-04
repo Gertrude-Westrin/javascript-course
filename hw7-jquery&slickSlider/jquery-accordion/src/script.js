4f989518b72d2b6f642df4fa4c10cf3e2ddf4dc5
Gertrude Westrin <gertrude.westrin@gmail.com>
2020-05-04 11:47:23 +0200

    Accordion homework complete

---
+++ hw7-jquery&slickSlider/jquery-accordion/src/script.js
@@ -0,0 +1,32 @@
+$('.locationText').hide();
+$('#sectionTitleLocation').on('click', () => {
+    $('.locationText').toggle();
+}
+);
+
+
+$('.musicText').hide();
+$('#sectionTitleMusic').on('click', () => {
+    $('.musicText').toggle();
+}
+);
+
+
+$('.notesText').hide();
+$('#sectionTitleNotes').on('click', () => {
+    $('.notesText').toggle();
+}
+);
+
+$('.booksText').hide();
+$('#sectionTitleBooks').on('click', () => {
+    $('.booksText').toggle();
+}
+);
+
+
+$('.tendanciesText').hide();
+$('#sectionTitleTendancies').on('click', () => {
+    $('.tendanciesText').toggle();
+}
+);
